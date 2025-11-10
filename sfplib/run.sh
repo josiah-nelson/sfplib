70d3d08d3e7c6d6f5d5fa4b86973578613a35dec
Josiah Nelson <josiah@jnel.dev>
2025-11-10 22:14:12 +0000

    Fix run.sh to exec /init for s6-overlay

--- sfplib/run.sh
+++ sfplib/run.sh
@@ -1,16 +1,11 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/env bashio
 
 # Create directories early
 bashio::log.info "Creating data directories..."
 mkdir -p /config/sfplib/submissions
 mkdir -p /config/sfplib/backups
 
-# Set deployment mode to HA addon
-export DEPLOYMENT_MODE="homeassistant"
-export HA_ADDON_MODE="true"
-
 bashio::log.info "Starting SFPLiberate Home Assistant Add-On..."
-bashio::log.info "Configuration will be loaded by backend service"
 
-# Note: Environment variables are exported by /etc/services.d/backend/run
-# This avoids duplication and ensures configuration is loaded just-in-time
+# Execute s6-overlay init system
+exec /init
